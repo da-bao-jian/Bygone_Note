d0e27f1207a73aa7079f2770087680cd4f10156c
Harry Gao <gaohaoyuandd@gmail.com>
2020-11-19 15:23:38 -0500

    changed create to create! in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-demo_user = User.create([{email: 'demo@gmail.com', password: '123456'}])
+demo_user = User.create!([{email: 'demo@gmail.com', password: '123456'}])
