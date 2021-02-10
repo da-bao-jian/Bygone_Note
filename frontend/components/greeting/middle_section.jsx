cc9fd91fb2261cacd99c3f050fad945f35655e1f
harry <gaohaoyuandd@gmail.com>
2021-02-10 17:34:21 -0500

    added gifs

--- frontend/components/greeting/middle_section.jsx
+++ frontend/components/greeting/middle_section.jsx
@@ -10,16 +10,24 @@ const Middle = () => {
         <div className='gretting-page'>
             <div className='middle'>
                 {!hover1 && !hover2 && !hover3 ? 
-                    <textarea  className='greeting-textarea' rows='26' cols='50' >
-                    “You mean old books?"   "Stories written before space travel but about space travel."                   "How could there have been stories about space travel before..."                    "The writers," Pris said, "made it up.”                                                        ― Philip K. Dick,               'Do Androids Dream of Electric Sheep?' 🐑
-                    </textarea> : null
+                    <div>
+                        <textarea  className='greeting-textarea' rows='26' cols='50' >
+                            Accomplish more with better notes:                                    Bygone Note helps you capture ideas and find them fast
+                        </textarea> 
+                        
+                        <div className='photo-container' rows='26' cols='50'>
+                            <div className='matrix'></div>
+                        </div>
+                    </div>
+                    : null
                 }
                 {hover1 ? 
                 <div>
-                    <textarea  className='greeting-textarea' rows='26' cols='50' >
+                    <textarea  className='greeting-textarea1' rows='26' cols='50' >
+                        Imrproved upon the original Evernote, Bygone Note uses Websocket for instant autosave that reduces delays.
                     </textarea>
                     <div className='photo-container' rows='26' cols='50'>
-                         <div className='stock-photos'></div>
+                         <div className='cyberpunk'></div>
 
                     </div>
                 </div> : null
@@ -27,7 +35,7 @@ const Middle = () => {
                 {hover2 ? 
                 <div>
                     <textarea  className='greeting-textarea' rows='26' cols='50' >
-                    “You mean old books?"   "Stories written before space travel but about space travel."                   "How could there have been stories about space travel before..."                    "The writers," Pris said, "made it up.”                                                        ― Philip K. Dick,               'Do Androids Dream of Electric Sheep?' 🐑
+                        Imrproved upon the original Evernote, Bygone Note takes advantage of algorithmic solution for its search feature 
                     </textarea>
                     <div className='photo-container' rows='26' cols='50'>
                          <div className=''></div>
@@ -40,14 +48,14 @@ const Middle = () => {
                     “You mean old books?"   "Stories written before space travel but about space travel."                   "How could there have been stories about space travel before..."                    "The writers," Pris said, "made it up.”                                                        ― Philip K. Dick,               'Do Androids Dream of Electric Sheep?' 🐑
                     </textarea>
                     <div className='photo-container' rows='26' cols='50'>
-                         <div className='stock-photos'></div>
+                         <div className=''></div>
 
                     </div>
                 </div> : null}
             </div>
             <div className='side-list'>
-                <button className='selection' ref={node1}>Neon PC</button>
-                <button className='selection' ref={node2}>Features</button>
+                <button className='selection1' ref={node1}>Autosave</button>
+                <button className='selection' ref={node2}>Search</button>
                 <button className='selection' ref={node3}>Styled</button>
             </div>
             <div>
@@ -58,6 +66,7 @@ const Middle = () => {
 };
 
 function useHover(){
+
     const node = useRef();
 
     const [hover, setHover] = useState(false);
